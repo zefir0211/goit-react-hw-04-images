67f42dcd010ff2a50219f6eb1fa16cf07912dd03
Dmytro Savitskyi <zefir0211@gmail.com>
2023-08-14 12:06:47 +0200

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,89 @@
-import React, { useEffect } from 'react';
-import { createPortal } from "react-dom";
+// import React, { useEffect } from 'react';
+// import { createPortal } from "react-dom";
+// import PropTypes from 'prop-types';
+// import { BackDrop, ModalWindow, Img } from './Modal.styled'
+
+// const Modal = ({ src, alt, popap }) => {
+//     useEffect(() => {
+//         window.addEventListener('keydown', clickEscape);
+//         return () => {
+//         window.removeEventListener('keydown', clickEscape);
+//     };
+//     }, []);
+
+//     const clickEscape = event => {
+//         if (event.code !== 'Escape') {
+//         return;
+//         }
+//         popap();
+//     };
+
+//     const clickModal = event => {
+//         if (event.target !== event.currentTarget) {
+//         return;
+//         }
+//         popap();
+//     };
+//         return createPortal(
+//             <BackDrop>
+//                 <ModalWindow onClick={clickModal}>
+//                     <Img src={src} alt={alt}></Img>
+//                 </ModalWindow>
+//             </BackDrop>,
+//         document.querySelector('#modal-root')
+//         );
+// };
+
+// Modal.propTypes = {
+//     src: PropTypes.string.isRequired,
+//     alt: PropTypes.string.isRequired,
+//     popap: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
+
+import React, { useEffect, useCallback } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
-import { BackDrop, ModalWindow, Img } from './Modal.styled'
+import { BackDrop, ModalWindow, Img } from './Modal.styled';
 
 const Modal = ({ src, alt, popap }) => {
+    const clickEscape = useCallback(
+        event => {
+            if (event.code !== 'Escape') {
+                return;
+            }
+            popap();
+        },
+        [popap]
+    );
+
     useEffect(() => {
         window.addEventListener('keydown', clickEscape);
+
         return () => {
             window.removeEventListener('keydown', clickEscape);
         };
-    }, []);
+    }, [clickEscape]);
 
-    const clickEscape = (event) => {
-        if (event.code !== 'Escape') {
-            return;
-        }
-        popap();
-    }
+    const clickModal = useCallback(
+        event => {
+            if (event.target !== event.currentTarget) {
+                return;
+            }
+            popap();
+        },
+        [popap]
+    );
 
-    const clickModal = (event) => {
-        if (event.target !== event.currentTarget) {
-            return;
-        }
-        popap();
-    }
-        return createPortal(
-            <BackDrop>
-                <ModalWindow onClick={clickModal}>
-                    <Img src={src} alt={alt}></Img>
-                </ModalWindow>
-            </BackDrop>,
+    return createPortal(
+        <BackDrop>
+            <ModalWindow onClick={clickModal}>
+                <Img src={src} alt={alt} />
+            </ModalWindow>
+        </BackDrop>,
         document.querySelector('#modal-root')
-        );
+    );
 };
 
 Modal.propTypes = {
